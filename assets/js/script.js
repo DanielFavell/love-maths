34c1577ad84c8824d0c1936b03b36b557bec70f5
Daniel Favell <danfavell97@gmail.com>
2024-01-25 14:40:28 +0000

    added division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,9 @@ else if(gameType === "multiply"){
 else if(gameType === "subtract"){
     displaySubtractQuestion(num1,num2);
 }
+else if (gameType === "division"){
+    displayDivisionQuestion(num1,num2);
+}
 else{
     alert(`unknown gametype ${gameType}`);
     throw `unknown gametype ${gameType}. aborting!`;
@@ -68,6 +71,9 @@ function calculateCorrectAnswer(){
     else if (operator === "-"){
         return [operand1 - operand2, "subtract"];
     }
+    else if(operator === "/"){
+        return [operand1 / operand2, "division"]
+    }
     else{
         alert(`unknown operator ${operator}`);
         throw `unknown operator ${operator}. aborting!`;
@@ -100,4 +106,10 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1*operand2;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
