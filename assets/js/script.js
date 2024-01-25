3d8bbf8881920786c443254b93939020bd5d4190
Daniel Favell <danfavell97@gmail.com>
2024-01-25 14:05:15 +0000

    added code to increment score and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,8 +31,10 @@ function checkAnswer(){
     let isCorrect = userAnswer === calculatedAnswer[0];
     if (isCorrect){
         alert("Hey! You got it right!")
+        incrementScore();
     }else{
         alert(`you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`)
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1])
 }
@@ -52,11 +54,13 @@ function calculateCorrectAnswer(){
 }
 
 function incrementScore(){
-
+let oldScore = parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
